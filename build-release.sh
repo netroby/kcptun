71e0003844279552179b5701003864ac9e971a83
ZhiFeng Hu <hufeng1987@gmail.com>
2016-11-17 14:50:21 +0800

    update  build-release.sh to build to build/ dir
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- build-release.sh
+++ build-release.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ ! -d build ];then
+    mkdir build
+fi
 MD5='md5sum'
 unamestr=`uname`
 if [[ "$unamestr" == 'Darwin' ]]; then
@@ -23,20 +26,20 @@ for os in ${OSES[@]}; do
 		then
 			suffix=".exe"
 		fi
-		env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o client_${os}_${arch}${suffix} github.com/xtaci/kcptun/client
-		env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o server_${os}_${arch}${suffix} github.com/xtaci/kcptun/server
-		if $UPX; then upx -9 client_${os}_${arch}${suffix} server_${os}_${arch}${suffix};fi
-		tar -zcf kcptun-${os}-${arch}-$VERSION.tar.gz client_${os}_${arch}${suffix} server_${os}_${arch}${suffix}
-		$MD5 kcptun-${os}-${arch}-$VERSION.tar.gz
+		env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o build/client_${os}_${arch}${suffix} ./client
+		env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o build/server_${os}_${arch}${suffix} ./server
+		if $UPX; then upx -9 build/client_${os}_${arch}${suffix} build/server_${os}_${arch}${suffix};fi
+		tar -zcf build/kcptun-${os}-${arch}-$VERSION.tar.gz build/client_${os}_${arch}${suffix} build/server_${os}_${arch}${suffix}
+		$MD5 build/kcptun-${os}-${arch}-$VERSION.tar.gz
 	done
 done
 
 # ARM
 ARMS=(5 6 7)
 for v in ${ARMS[@]}; do
-	env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=$v go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o client_linux_arm$v  github.com/xtaci/kcptun/client
-	env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=$v go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o server_linux_arm$v  github.com/xtaci/kcptun/server
+	env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=$v go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o build/client_linux_arm$v  ./client
+	env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=$v go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o build/server_linux_arm$v  ./server
 done
 if $UPX; then upx -9 client_linux_arm* server_linux_arm*;fi
-tar -zcf kcptun-linux-arm-$VERSION.tar.gz client_linux_arm* server_linux_arm*
-$MD5 kcptun-linux-arm-$VERSION.tar.gz
+tar -zcf build/kcptun-linux-arm-$VERSION.tar.gz build/client_linux_arm* build/server_linux_arm*
+$MD5 build/kcptun-linux-arm-$VERSION.tar.gz
